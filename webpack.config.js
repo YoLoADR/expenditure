b9c729c61fcc530e15a99e3547f584aa3810e37a
13091987Y <atticot.yohann@gmail.com>
2018-01-02 11:37:00 +0100

    iniate Project Expenditure

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'file-loader?name=[name].[ext]',
+      },
+      {
+        loader: 'babel-loader',
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }, {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+};
