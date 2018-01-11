21734025e48b2abf90b09b8b049d161277e98358
13091987Y <atticot.yohann@gmail.com>
2018-01-11 14:46:51 +0100

    Rendering Individual Expenses

--- src/app.js
+++ src/app.js
@@ -12,12 +12,12 @@ import { setTimeout } from 'timers';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(addExpense({ description: 'Water bill', amount: '133', createdAt: Date.now() }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: '157', createdAt: Date.now() + 3 }));
 store.dispatch(setTextFilter('water'));
 
 setTimeout(()=>{
-    store.dispatch(setTextFilter('Holla Yohann !'));
+    store.dispatch(setTextFilter('bill'));
 }, 3000);
 
 const state = store.getState();
