172a3d251b724acbf49b53de3cd582a9c4ec475e
13091987Y <atticot.yohann@gmail.com>
2018-01-22 14:11:44 +0100

    setup production and server

--- src/app.js
+++ src/app.js
@@ -1,33 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouters from './routers/AppRouters';
+import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
-import { setTimeout } from 'timers';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: '133', createdAt: Date.now() }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: '157', createdAt: Date.now() + 3 }));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(()=>{
-    store.dispatch(setTextFilter('bill'));
-}, 3000);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
-    <Provider store={store}>
-        <AppRouters />
-    </Provider>
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,49 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        loader: 'file-loader?name=[name].[ext]',
-      },
-      {
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return {
+    entry: './src/app.js',
+    output: {
+      publicPath: '/',
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
         loader: 'babel-loader',
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
+        test: /\.js$/,
+        exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
